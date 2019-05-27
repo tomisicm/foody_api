43d71ada249d3a3ad7d71637cefa4bd2316f3d3f
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-27 20:18:22 +0200

    and another one

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ import { connect } from './utils/db'
 import userRouter from './resources/user/user.router'
 import CateringEstablishmentRouter from './resources/CateringEstablishment/CateringEstablishment.router'
 import CuisineRouter from './resources/food/cuisine.router'
-import ReviewRouter from './resources/food/review.router'
+import ReviewRouter from './resources/review/review.router'
 
 Joi.objectId = require('joi-objectid')(Joi)
 
