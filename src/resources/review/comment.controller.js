4eba699be69404a947dd06da6325a722b0f7d98a
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-05-30 11:36:22 +0200

    comment sorting, create comment,

--- src/resources/review/comment.controller.js
+++ src/resources/review/comment.controller.js
@@ -8,7 +8,7 @@ const getCommentsByItemId = async (req, res) => {
       path: 'createdBy',
       select: '_id name'
     },
-    sort: 'createdAt',
+    sort: '-createdAt',
     page: parseInt(page, 10) || 1,
     limit: parseInt(perPage, 10) || 10,
     lean: true
@@ -22,4 +22,41 @@ const getCommentsByItemId = async (req, res) => {
   }
 }
 
-export default { getCommentsByItemId }
+const createComment = async (req, res) => {
+  const createdBy = req.user._id
+
+  try {
+    const doc = await Comment.create({ ...req.body, createdBy })
+    res.status(201).json({ data: doc })
+  } catch (e) {
+    console.error(e)
+    res.status(400).send(e)
+  }
+}
+
+const editComment = async (req, res) => {
+  try {
+    /* const updatedDoc = await Comment.findOneAndUpdate(
+      {
+        createdBy: req.user._id,
+        _id: req.params.id
+      },
+      req.body,
+      { new: true }
+    )
+      .lean()
+      .exec() */
+    console.log(req)
+
+    /* if (!updatedDoc) {
+      return res.status(400).end()
+    }
+
+    res.status(200).json({ data: updatedDoc }) */
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+export default { getCommentsByItemId, createComment, editComment }
--- src/resources/review/comment.router.js
+++ src/resources/review/comment.router.js
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import controllers from './comment.controller'
+import { protect } from '../../utils/auth'
 
 const router = Router()
+
+router.route('/').post(protect, controllers.createComment)
 
 router.route('/item/:itemId').get(controllers.getCommentsByItemId)
 
