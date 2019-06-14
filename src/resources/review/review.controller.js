76eb81816bf496d3edf6c04fe21efa209ee34866
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-06-14 17:34:54 +0200

    populate review createdBy

--- src/resources/review/review.controller.js
+++ src/resources/review/review.controller.js
@@ -177,7 +177,10 @@ export const getReviewsByItemId = async (req, res) => {
 
 export const getReviewById = async (req, res) => {
   try {
-    const doc = await Review.findById(req.params.id)
+    const doc = await Review.findById(req.params.id).populate({
+      path: 'createdBy',
+      select: '_id name'
+    })
     res.status(200).json({ data: doc })
   } catch (e) {
     console.error(e)
