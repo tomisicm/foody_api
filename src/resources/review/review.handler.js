3725bec6637555d7c702c8cc64693b8bfb3f1092
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-07-11 09:52:58 +0200

    calc and update avg rating

--- src/resources/review/review.handler.js
+++ src/resources/review/review.handler.js
@@ -1,7 +1,38 @@
 import { reviewHandler } from './reveiw.emitter'
 
+// import mongoose from 'mongoose'
+
+import { Review } from './review.model'
+import { CateringEstablishment } from './../CateringEstablishment/catering.model'
+
 const recalculate = function() {
-  reviewHandler.on('dick', data => console.log(data))
+  reviewHandler.on('dick', async function(data) {
+    /*
+    const ddoc = await mongoose
+      .model('cateringestablishment')
+      .findById(data.item)
+    */
+
+    // ?might be static fucntion
+    let docc = await Review.aggregate([
+      {
+        $match: {
+          item: data.item
+        }
+      },
+      {
+        $group: {
+          _id: null,
+          rating: {
+            $avg: '$avgRating'
+          }
+        }
+      },
+      { $project: { _id: 0 } }
+    ])
+
+    await CateringEstablishment.findByIdAndUpdate(data.item, docc[0])
+  })
 }
 
 exports.recalculate = recalculate
