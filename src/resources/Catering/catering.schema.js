a09818b571e2fa531b354b9fe641bfbda4912ebc
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-07-25 09:29:23 +0200

    1 todo less

--- src/resources/Catering/catering.schema.js
+++ src/resources/Catering/catering.schema.js
@@ -1,51 +1,48 @@
 import Joi from '@hapi/joi'
 
 import { addressShema } from '../../commonSchemas/address.schema'
+
 Joi.objectId = require('joi-objectid')(Joi)
 
 const name = Joi.string()
   .trim()
   .max(50)
 
-// TODO: refactor common fields
-export const createObjectSchema = Joi.object()
-  .options({ stripUnknown: true })
-  .keys({
-    name: name.required(),
-    address: addressShema,
-    contactInformation: Joi.array().items(
-      Joi.string().email(),
-      Joi.string().regex(
-        // eslint-disable-next-line no-useless-escape
-        /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
-        { name: 'phone number' }
-      )
-    ),
-    website: Joi.string().allow(''),
-    cuisine: Joi.array(),
-    michelinStars: Joi.number().default(0),
-    // owner: Joi.objectId(),
-    pageMaintainedBy: Joi.array().items(Joi.objectId()),
-    images: Joi.array().items(Joi.string())
-  })
+const contactInformation = Joi.array().items(
+  Joi.string().email(),
+  Joi.string().regex(
+    // eslint-disable-next-line no-useless-escape
+    /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
+    { name: 'phone number' }
+  )
+)
 
-export const editObjectSchema = Joi.object()
-  .options({ stripUnknown: true })
-  .keys({
-    name: name.required(),
-    address: addressShema,
-    contactInformation: Joi.array().items(
-      Joi.string().email(),
-      Joi.string().regex(
-        // eslint-disable-next-line no-useless-escape
-        /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
-        { name: 'phone number' }
-      )
-    ),
-    website: Joi.string().allow(''),
-    cuisine: Joi.array(),
-    michelinStars: Joi.number().default(0),
-    /* owner: Joi.objectId(), */
-    pageMaintainedBy: Joi.array().items(Joi.objectId()),
-    images: Joi.array().items(Joi.string())
-  })
+const cuisine = Joi.array()
+const website = Joi.string().allow('')
+const michelinStars = Joi.number().default(0)
+const pageMaintainedBy = Joi.array().items(Joi.objectId())
+const images = Joi.array().items(Joi.string())
+
+export const createObjectSchema = Joi.object().keys({
+  name: name.required(),
+  address: addressShema,
+  contactInformation: contactInformation,
+  website: website,
+  cuisine: cuisine,
+  michelinStars: michelinStars,
+  // owner: Joi.objectId(),
+  pageMaintainedBy: pageMaintainedBy,
+  images: images
+})
+
+export const editObjectSchema = Joi.object().keys({
+  name: name,
+  address: addressShema,
+  contactInformation: contactInformation,
+  website: website,
+  cuisine: cuisine,
+  michelinStars: michelinStars,
+  /* owner: Joi.objectId(), */
+  pageMaintainedBy: pageMaintainedBy,
+  images: images
+})
