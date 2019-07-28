26f8a20797987110483a7df96e10366fd0f3d07a
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-07-28 15:25:35 +0200

    ayyy

--- src/resources/statistics/statisticService.js
+++ src/resources/statistics/statisticService.js
@@ -5,11 +5,20 @@ import MODEL from '../models'
 
 class Statistics {
   async countNewlyCreatedDocuments(model) {
-    const doc = await mongoose.model(MODEL.REVIEW).find({
-      createdAt: {
-        $gte: moment().startOf('week')
+    const doc = await mongoose.model(MODEL.REVIEW).aggregate([
+      {
+        $match: {
+          createdAt: {
+            $gte: moment()
+              .startOf('week')
+              .toDate()
+          }
+        }
+      },
+      {
+        $count: 'weeklyReviewStatistic'
       }
-    })
+    ])
     return doc
   }
 }
